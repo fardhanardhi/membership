9fb9f95d574e805dc5fe6456c969f17402537fea
chintyadewi <chintyadewi437@gmail.com>
2020-01-18 11:22:23 +0700

    Add item profile setting

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import LoadingIndicator from './LoadingIndicator';
 import Voucher from './Voucher';
 import HistoryItem from './HistoryItem';
-import ProfileItem from './ProfileItem';
 
-export {LoadingIndicator, Voucher, HistoryItem, ProfileItem};
+export {LoadingIndicator, Voucher, HistoryItem};
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {SafeAreaView, View, Text, Image} from 'react-native';
+import {SafeAreaView, View, Text, Image, TouchableOpacity} from 'react-native';
 import {
   Appbar,
   Avatar,
@@ -7,21 +7,36 @@ import {
   Subheading,
   Title,
   List,
+  Divider,
 } from 'react-native-paper';
 import Feather from 'react-native-vector-icons/Feather';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import {ScrollView} from 'react-native-gesture-handler';
-import {ProfileItem} from '../components';
+import {GoogleSignin} from '@react-native-community/google-signin';
 
 export default class ProfileScreen extends Component {
   static navigationOptions = {
     headerShown: false,
   };
 
+  componentDidMount() {
+    GoogleSignin.configure();
+  }
+
+  signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      this.props.navigation.navigate('AuthLoading');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   render() {
     return (
-      <SafeAreaView>
+      <SafeAreaView style={{flex: 1}}>
         <Appbar.Header>
           <Appbar.BackAction onPress={() => this.props.navigation.goBack()} />
           <Appbar.Content title="Profile" />
@@ -31,7 +46,6 @@ export default class ProfileScreen extends Component {
           <View
             style={{
               backgroundColor: Colors.white,
-              flex: 1,
               justifyContent: 'center',
               alignItems: 'center',
               padding: 30,
@@ -107,11 +121,68 @@ export default class ProfileScreen extends Component {
 
           <View style={styles.viewSubContent}>
             <Subheading style={styles.profileItem}>Account</Subheading>
-            <ProfileItem
-              icon={() => <Fontisto name="at" size={18} />}
-              title={'Change Username'}
+            <List.Item
+              title="Change Username"
+              left={props => (
+                <List.Icon
+                  {...props}
+                  icon={() => <Fontisto name="at" size={18} />}
+                />
+              )}
+              right={props => <List.Icon {...props} icon="chevron-right" />}
+            />
+            <Divider style={{marginHorizontal: 20}} />
+            <List.Item
+              title="Change Password"
+              left={props => (
+                <List.Icon
+                  {...props}
+                  icon={() => <Fontisto name="locked" size={18} />}
+                />
+              )}
+              right={props => <List.Icon {...props} icon="chevron-right" />}
             />
           </View>
+
+          <View style={styles.viewSubContent}>
+            <Subheading style={styles.profileItem}>About</Subheading>
+            <List.Item
+              title="Version: 1.0"
+              left={props => (
+                <List.Icon
+                  {...props}
+                  icon={() => <FontAwesome name="info-circle" size={20} />}
+                />
+              )}
+            />
+            <Divider style={{marginHorizontal: 20}} />
+            <List.Item
+              title="Help"
+              left={props => (
+                <List.Icon
+                  {...props}
+                  icon={() => <FontAwesome name="question-circle" size={20} />}
+                />
+              )}
+              right={props => <List.Icon {...props} icon="chevron-right" />}
+            />
+          </View>
+
+          <TouchableOpacity onPress={this.signOut}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: Colors.white,
+                marginBottom: 10,
+                paddingTop: 10,
+                paddingBottom: 10,
+              }}>
+              <List.Item
+                title="Logout"
+                left={props => <List.Icon {...props} icon="logout" />}
+              />
+            </View>
+          </TouchableOpacity>
         </ScrollView>
       </SafeAreaView>
     );
@@ -120,10 +191,15 @@ export default class ProfileScreen extends Component {
 
 const styles = {
   profileItem: {
-    marginTop: 22,
     marginLeft: 22,
-    marginBottom: 0,
+    marginBottom: 5,
     fontWeight: 'bold',
   },
-  viewSubContent: {flex: 1, backgroundColor: Colors.white, marginBottom: 10},
+  viewSubContent: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    marginBottom: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
 };
